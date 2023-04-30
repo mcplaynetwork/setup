ebe0fa122ff111a32b5da77abb0a2c03aa98f51a
snake <admin@viasnake.com>
2023-04-30 12:50:15 +0900

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,31 @@
+echo "############ Setup docker ############"
+
+sudo apt-get update
+sudo apt-get install \
+ca-certificates \
+curl \
+gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+echo "############ Install docker ############"
+
+sudo apt-get update
+sudo apt-get install \
+docker-ce \
+docker-ce-cli \
+containerd.io \
+docker-buildx-plugin \
+docker-compose-plugin
+
+echo "############ apt-get upgrade ############"
+
+sudo apt-get upgrade
+
